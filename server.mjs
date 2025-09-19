5e21c3d87513b241bcaa36cc94c14e8b1c90aaca
Jason Chao <jason.chao@infrontconsulting.com.my>
2025-09-19 17:45:44 +0800

    update yaml

---
+++ server.mjs
@@ -0,0 +1,21 @@
+// server.mjs
+import { createServer } from 'http'
+import { parse } from 'url'
+import next from 'next'
+
+const dev = process.env.NODE_ENV !== 'production'
+const hostname = '0.0.0.0'
+const port = process.env.PORT || 3000
+
+const app = next({ dev, hostname, port })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    const parsedUrl = parse(req.url, true)
+    await handle(req, res, parsedUrl)
+  }).listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://${hostname}:${port}`)
+  })
+})
